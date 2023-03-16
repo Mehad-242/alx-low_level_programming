0d148f163cf364c979135b938e16ba10c9036096
Mehad-242 <mehadboosh@gmail.com>
2023-03-16 16:44:19 -0700

    hello, pust

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
