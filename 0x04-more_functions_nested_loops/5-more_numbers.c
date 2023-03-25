9f1c8759290e0f95785813b3744acc1e24c0d494
Mehad-242 <mehadboosh@gmail.com>
2023-03-24 17:43:45 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,14 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
-		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar (j % 10 + '0');
-		}
-		_putchar('\n');
+	for (j = 0; j <= 14; j++)
+	{
+	if (j > 10)
+	{
+	_putchar((j / 10) + '0');
+	}
+	_putchar((j % 10) + '0');
+	}
+	_putchar('\n');
 	}
 }
