66e7094d4f8183c726a43a2e5399a4d3f137fa85
Mehad-242 <mehadboosh@gmail.com>
2023-03-30 19:56:59 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer parametars
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		_putchar('_');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n % 10) + '0');
+}
