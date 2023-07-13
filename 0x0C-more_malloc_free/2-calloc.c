cbb75990f2d0ad12c380016580594ef4d982c537
Mehad-242 <mehadboosh@gmail.com>
2023-07-13 13:20:46 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ char *_memset(char *s, char b, unsigned int n)
 	return (s);
 }
 /**
- * -calloc - allocates memory for an array
+ * *_calloc - allocates memory for an array
  *  @nmemb: number of elments in the arry
  *  @size: size of each element
  *
