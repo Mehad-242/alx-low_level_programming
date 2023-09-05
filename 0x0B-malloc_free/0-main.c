e317bea67c95ef82b8455d11b82c6ed8370a1cc6
Mehad-242 <mehadboosh@gmail.com>
2023-09-05 07:59:28 -0700

    a

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -17,12 +17,12 @@ void simple_print_buffer(char *buffer, unsigned int size)
 	while (i < size)
 	{
 		if (i % 10)
-        	{
+		{
 			printf(" ");
 		}
 		if (!(i % 10) && i)
-        	{
-            		printf("\n");
+		{
+			printf("\n");
 		}
 		printf("0x%02x", buffer[i]);
 		i++;
