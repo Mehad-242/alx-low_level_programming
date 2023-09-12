e3310a39b7b9c9f9b5f3ac6e2c183685f3bb285b
Mehad-242 <mehadboosh@gmail.com>
2023-09-12 16:53:28 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: jast a lone dog structer in a big kitty world
+ */
+
+struct dog
+
+
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
