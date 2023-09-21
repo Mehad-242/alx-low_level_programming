533ffc892239ce6dc870fac315bb1f76857a9cac
Mehad-242 <mehadboosh@gmail.com>
2023-09-21 15:04:05 -0700

    b

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * size_t list_len - returns the number of elements in a linked list
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
