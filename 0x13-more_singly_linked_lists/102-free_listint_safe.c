c04e3bc8ae00c140694aa9f9fa944f76148544ee
Mehad-242 <mehadboosh@gmail.com>
2023-09-26 14:19:50 -0700

    n

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,64 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * find_listint__loop_fl - finds a loop in a linked list
+ * @head: linked list to search
+ *
+ * Return: address of node where loop starts
+ */
+listint_t *find_listint_loop_fl(listint_t *head)
+{
+	listint_t *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
+
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h:
+ *
+ * Return: the size of the list that was free’d
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *next, *loopnode;
+	size_t lenth;
+	int loop = 1;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	loopnode = find_listint_loop_fl(*h);
+	for (lenth = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
+	{
+		lenth++;
+		next = (*h)->next;
+		if (*h == loopnode && loop)
+		{
+			if (loopnode == loopnode->next)
+			{
+				free(*h);
+				break;
+			}
+			lenth++;
+			next = next->next;
+			free((*h)->next);
+			loop = 0;
+		}
+		free(*h);
+	}
+	*h = NULL;
+	return (lenth);
+}
