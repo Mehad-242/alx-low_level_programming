149ed95b123640ca3cf84b87c6c30eb6e0a2dc22
Mehad-242 <mehadboosh@gmail.com>
2023-10-02 17:03:59 -0700

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,14 +12,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes;
-	char buf[EAD_BUF_SIZE * 8];
+	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
-	fd = open(filename, O_RDONL Y);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	bytes = read(fd, &buf[0], lettes);
+	bytes = read(fd, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
 	return (bytes);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define READ_BUF_SIZE 1024
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
