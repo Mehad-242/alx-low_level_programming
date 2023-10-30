ee87b20579c460580bd6b980806caf6b6e35a4c0
Mehad-242.com <mehadboosh@gmail.com>
2023-10-30 12:27:36 -0700

    c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,9 +31,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | 0_TRUNC, S_IRUSR | S_IWUSR)
-		if (fd == -1)
-			return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | 0_TRUNC,
+			S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 	if (len)
 		byt = write(fd, text_content, len);
 	close(fd);
