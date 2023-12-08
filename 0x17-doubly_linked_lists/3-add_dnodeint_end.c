77b9b6726b5e18f1153c4babae02d46980a335c9
Mehad-242.com <mehadboosh@gmail.com>
2023-12-08 12:13:03 -0800

    a

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -17,7 +17,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->next = NULL;
-	if (!head)
+	if (!*head)
 	{
 		new->prev = NULL;
 		*head = new;
--- 0x17-doubly_linked_lists/3-main.c
+++ 0x17-doubly_linked_lists/3-main.c
@@ -23,5 +23,4 @@ int main(void)
 	add_dnodeint_end(&head, 1024);
 	print_dlistint(head);
 	return (EXIT_SUCCESS);
-        sum += int(sys.argv[i + 1])
 }
