392a8a0e506231c821a8818a8fcc1f3d751e1efd
Mehad-242.com <mehadboosh@gmail.com>
2023-12-08 08:38:06 -0800

    a

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data (n)
+ * @head: pointer to current head node
+ *
+ * Return: int sum of values
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
