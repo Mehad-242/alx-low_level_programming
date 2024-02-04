6e199e192de1cdc5765db178dc360df0a01087ad
Mehad-242 <mehadboosh@gmail.com>
2024-02-04 13:37:05 -0800

    a

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the index of a key
+ * @key: key to get index for
+ * @size: size of the hash table
+ *
+ * Return: index for the key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
